ac294e3bb889296833bd1171fb657f08840711b3
marvelperseus <marvelperseus@outlook.com>
2019-01-09 01:21:08 -0800

    angular7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,49 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductsComponent } from './products/products.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductAddComponent } from './product-add/product-add.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
+import { from } from 'rxjs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatInputModule,
+  MatPaginatorModule,
+  MatProgressSpinnerModule,
+  MatSortModule,
+  MatTableModule,
+  MatIconModule,
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule } from "@angular/material";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    ProductDetailComponent,
+    ProductAddComponent,
+    ProductEditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatProgressSpinnerModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/product-add/product-add.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
+import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-product-add',
+  templateUrl: './product-add.component.html',
+  styleUrls: ['./product-add.component.scss']
+})
+export class ProductAddComponent implements OnInit {
+
+  productForm: FormGroup;
+  prod_name:string='';
+  prod_desc:string='';
+  prod_price:number=null;
+  updated_at:Date=null;
+  isLoadingResults = false;
+
+  constructor(private router: Router, private api: ApiService, private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.productForm = this.formBuilder.group({
+      'prod_name' : [null, Validators.required],
+      'prod_desc' : [null, Validators.required],
+      'prod_price' : [null, Validators.required],
+      'updated_at' : [null, Validators.required]
+    });
+  }
+
+  onFormSubmit(form:NgForm) {
+    this.isLoadingResults = true;
+    this.api.addProduct(form)
+      .subscribe(res => {
+          let id = res['id'];
+          this.isLoadingResults = false;
+          this.router.navigate(['/product-details', id]);
+        }, (err) => {
+          console.log(err);
+          this.isLoadingResults = false;
+        });
+  }
+
+}
